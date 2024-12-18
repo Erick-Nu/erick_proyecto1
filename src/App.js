25f333fc6e699e5ec64bc46c4c4e7f054ab0d846
Erick-Nu <erick.nunez02@epn.edu.ec>
2024-12-17 22:13:04 -0500

    React with Vite Proyect

--- src/App.js
+++ src/App.js
@@ -12,38 +12,11 @@ function App() {
         <li><strong>Github:</strong> @Erick-Nu </li>
       </ul>
 
-      <h2>Completed Studies</h2>
-      <ol>
-        <li><strong>Escuela:</strong> "Ciudad de Cuenca"  </li>
-        <li><strong>Aprobada:</strong> True  </li>
-        <li><strong>Colegio:</strong> "Simon Bolivar" -- Bachillerato Tecnico </li>
-        <li><strong>Aprobada:</strong> True  </li>
-        <li><strong>Universidad:</strong> "Escuela Politecnica Nacional" -- Tecnologia en Desarrollo De Software </li>
-      </ol>
 
-      <h2>Tools Used</h2>
-      <ul>
-        <li>Visual Studio Code</li>
-        <li>Git</li>
-        <li>React</li>
-        <li>Node.js</li>
-      </ul>
 
-      <h2> Favorite sports </h2>
-      <div className="gallery">
-        <div className="gallery-item">
-          <img src="soccer.png" alt="soccer" />
-          <p> SOCCERBALL </p>
-        </div>
-        <div className="gallery-item">
-          <img src="volleyball.png" alt="volleyball" />
-          <p> VOLLEYBALL </p>
-        </div>
-        <div className="gallery-item">
-          <img src="basketball.png" alt="basketball" />
-          <p>BASKETBALL </p>
-        </div>
-      </div>
+
+
+
     </div>
   );
 }
